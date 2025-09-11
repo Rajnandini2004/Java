ffa50d08e5cd7d8b9b643e17be43e1f615a6408e
Rajnandini2004 <133628482+Rajnandini2004@users.noreply.github.com>
2025-09-11 22:58:23 +0530

    Add files via upload

---
+++ armstrong.java
@@ -0,0 +1,21 @@
+public class armstrong {
+    
+    public static void main(String args[])
+    {
+     int n = 153, r, sum = 0, temp = n;
+        while(n!=0)
+        {
+            r = n % 10;
+            sum = sum + (r*r*r);
+            n = n / 10;
+        }
+        if(sum == temp)
+        {
+            System.out.print("Armstrong");
+        }
+        else{
+            System.out.print("Not Armstrong");
+        }
+
+    }
+}
