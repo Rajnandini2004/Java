7b1eebb02b92d61680f3f8918af01a107d8eedfd
Rajnandini2004 <133628482+Rajnandini2004@users.noreply.github.com>
2025-09-11 23:00:38 +0530

    Add files via upload

---
+++ multiIntialization.java
@@ -0,0 +1,11 @@
+public class multiIntialization {
+    //do condition ke liye AND  ya OR gate ka use krte
+    public static void main(String args[])
+    {
+        int i, j;
+        for(i =1, j =10 ; i<j; i++, j--)
+        {
+            System.out.println(i + " " + j + " ");
+        }
+    }
+}
