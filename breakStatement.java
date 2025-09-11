872b40f15dc8bc6c4c45a4745a988253ce43cb67
Rajnandini2004 <133628482+Rajnandini2004@users.noreply.github.com>
2025-09-11 22:59:16 +0530

    Add files via upload

---
+++ breakStatement.java
@@ -0,0 +1,16 @@
+public class breakStatement {
+    public static void main(String args[])
+    {
+        int i =1;
+        for( ; ;)
+        {
+            if(i>10)
+            {
+                break;
+            }
+            System.out.print(i + " ");
+            i++;
+        }
+    }
+
+}
